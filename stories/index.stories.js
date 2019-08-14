5980ec50e857efbd682b720b3928229869ff6f8c
yes <yes@emq.com>
2019-08-14 17:06:18 +0800

    Add TimezoneNameSelect

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -2,13 +2,18 @@ import React from "react";
 
 import { storiesOf } from "@storybook/react";
 import styled from "styled-components";
+import "antd/dist/antd.css";
+
 import { BasicInputDemo } from "../components/Input/BasicInput";
 import FAIcon from "../components/FAIcon";
+import TimezoneNameSelect from "../components/Select/TimezoneNameSelect";
 import { MoneyInputDemo } from "../components/Input/MoneyInput";
 import "../styles/global-style.css";
+
 const Layout = styled.div`
   padding: 15px;
 `;
+
 storiesOf("Input", module)
   .add("BasicInput", () => (
     <Layout>
@@ -20,4 +25,11 @@ storiesOf("Input", module)
       <MoneyInputDemo />
     </Layout>
   ));
+
 storiesOf("Icon", module).add("FontAwesomeIcon", () => <FAIcon icon="cog" />);
+
+storiesOf("Select", module).add("TimezoneNameSelect", () => (
+  <Layout>
+    <TimezoneNameSelect />
+  </Layout>
+));
