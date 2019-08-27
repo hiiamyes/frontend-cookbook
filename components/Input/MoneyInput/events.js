355d270e35338011b4f9f9bd68f59007a4b56473
yes <yes@emq.com>
2019-08-27 16:57:07 +0800

    Update MoneyInput

--- components/Input/MoneyInput/events.js
+++ components/Input/MoneyInput/events.js
@@ -54,7 +54,7 @@ const onInputKeyDown = ({
   ) {
     e.preventDefault();
   }
-  if (e.key === "." && /\./.test(value)) {
+  if (e.key === "." && (!value || /\./.test(value))) {
     e.preventDefault();
   }
   // Get the chars before and after the caret before the change event.
