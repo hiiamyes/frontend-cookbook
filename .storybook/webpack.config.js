ffb792b021ad69849c1103fa7dbeab6870e41891
yes <yes@emq.com>
2019-10-09 13:04:38 +0800

    Add typescript

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,16 +1,22 @@
 // https://storybook.js.org/docs/configurations/custom-webpack-config/
 const path = require("path");
 
-// Export a function. Accept the base config as the only param.
 module.exports = async ({ config, mode }) => {
-  // `mode` has a value of 'DEVELOPMENT' or 'PRODUCTION'
-  // You can change the configuration based on that.
-  // 'PRODUCTION' is used when building the static version of storybook.
-
-  // Make whatever fine-grained changes you need
+  config.module.rules.push({
+    test: /\.(ts|tsx)$/,
+    use: [
+      {
+        loader: require.resolve("awesome-typescript-loader")
+      },
+      // Optional
+      {
+        loader: require.resolve("react-docgen-typescript-loader")
+      }
+    ]
+  });
+  config.resolve.extensions.push(".ts", ".tsx");
   config.resolve.alias = {
     src: path.resolve(__dirname, "../src/")
   };
-  // Return the altered config
   return config;
 };
