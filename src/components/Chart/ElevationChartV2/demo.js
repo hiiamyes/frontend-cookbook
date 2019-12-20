64b805466478884b30dfd8004758c44dfb3d0357
yes <yes@emq.com>
2019-12-20 18:15:17 +0800

    wip

--- src/components/Chart/ElevationChartV2/demo.js
+++ src/components/Chart/ElevationChartV2/demo.js
@@ -1,19 +1,13 @@
 import React from "react";
 import ElevationChartV2 from "./index";
-import hehuanWest from "./hehuan-west.json";
+import data from "./chilai-main-north-peaks.json";
 
 const ElevationChartV2Demo = () => {
+  const { paths, nodes } = data;
   return (
     <div>
       <h1>Hehuan West Peak</h1>
-      <ElevationChartV2
-        data={hehuanWest}
-        nodes={[
-          { distance: 2239, ele: 3413.555234243258, name: "合歡北峰" },
-          { distance: 6678, ele: 3137.1733986368426, name: "合歡西峰" },
-          { distance: 11117, ele: 3413.555234243258, name: "合歡北峰" }
-        ]}
-      />
+      <ElevationChartV2 data={paths} nodes={nodes} />
     </div>
   );
 };
