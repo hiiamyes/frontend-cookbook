321cbd1d320946bfeebcca27f3f1b39560ef7f86
Yes <joshuayes@gmail.com>
2020-02-11 02:19:22 +0800

    wip

--- src/components/Chart/ElevationChartV2/demo.js
+++ src/components/Chart/ElevationChartV2/demo.js
@@ -1,13 +1,20 @@
 import React from "react";
 import ElevationChartV2 from "./index";
 import data from "./chilai-main-north-peaks.json";
+import taoShanKelaye from "./tao-shan-kelaye.json";
 
 const ElevationChartV2Demo = () => {
   const { paths, nodes } = data;
   return (
     <div>
-      <h1>Hehuan West Peak</h1>
+      <h1>Chilai Main North Peaks</h1>
       <ElevationChartV2 data={paths} nodes={nodes} />
+
+      <h1>Tao Shan Kelaye</h1>
+      <ElevationChartV2
+        data={taoShanKelaye.paths}
+        nodes={taoShanKelaye.nodes}
+      />
     </div>
   );
 };
