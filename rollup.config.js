c50323114ec4c36d7d478054b5fccd6a69c5f3c6
Yes <joshuayes@gmail.com>
2020-05-31 10:07:18 +0800

    Update packages

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,9 @@
-import resolve from "rollup-plugin-node-resolve";
-// import external from "rollup-plugin-peer-deps-external";
-import commonjs from "rollup-plugin-commonjs";
-import babel from "rollup-plugin-babel";
+import path from "path";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import babel from "@rollup/plugin-babel";
 import alias from "@rollup/plugin-alias";
 import pkg from "./package.json";
-import path from "path";
-// const path = require("path");
 
 export default {
   input: "index.js",
@@ -18,7 +16,6 @@ export default {
   ],
   external: ["react", "styled-components"],
   plugins: [
-    // external(),
     resolve(),
     commonjs({
       include: "node_modules/**",
