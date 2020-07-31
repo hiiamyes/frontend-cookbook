c795f0261ad1ac77368df993dfafceda4d20aa44
Yes <joshuayes@gmail.com>
2020-07-31 23:48:26 +0800

    v1.0.9

--- dist/index.js
+++ dist/index.js
@@ -36053,5 +36053,75 @@ const Carousel = props => {
   }))));
 };
 
-export { Button, Carousel, FAIcon, Loader, Map$1 as Map, Modal, Trail, TrailChart, useModal };
+const teal500 = "#009688";
+const teal700 = "#00796b";
+const teal900 = "#004d40";
+const gray600 = "#757575";
+const baseTheme = {
+  height: "36px",
+  borderRadius: "4px",
+  padding: "0 16px",
+  colorText: "white",
+  colorEnable: teal700,
+  colorHover: teal500,
+  colorPressed: teal900,
+  colorFocused: teal500,
+  colorDisabled: gray600,
+  //
+  colorMainBlue: "#4357ef",
+  colorPrimary: "#07072d",
+  colorSecondary: "#545b64",
+  colorTertiary: "#a9b0b7",
+  colorPrimaryAlt: "#232658",
+  colorSecondaryAlt: "#8e8fa8",
+  colorTertiaryAlt: "#bdbdcc",
+  colorAssistive1: "#eeeef0",
+  colorAssistive2: "#cbcdd0",
+  colorAssistive3: "#edeeef",
+  colorAssistive4: "#eceefd",
+  colorRed: "#ff4c33",
+  colorGreen: "#28b76c",
+  colorPurple: "#5b4ad8",
+  colorOrange: "#f4bf0e",
+  colorWhite: "#ffffff",
+  colorPopup: "#ffffff",
+  backgroundColorApp: "rgba(84, 91, 100, 0.1)",
+  backgroundColorDivider: "#edeeef",
+  backgroundColorInput: "#ffffff",
+  backgroundColorTd: "#ffffff",
+  paddingInput: "0 14px",
+  paddingButton: "10px 0",
+  paddingButtonLarge: "21px 0",
+  heightNavBar: "67px",
+  heightInput: "37px",
+  heightLink: "37px",
+  heightButton: "37px",
+  borderRadius: "4px",
+  // Typography
+  fontSizeH1: "28px",
+  lineHeightH1: "36px",
+  fontSizeH2: "24px",
+  lineHeightH2: "30px",
+  fontSizeH3: "18px",
+  lineHeightH3: "23px",
+  fontSizeH4: "16px",
+  lineHeightH4: "21px",
+  fontSizeH5: "14px",
+  lineHeightH5: "18px",
+  fontSizeH6: "12px",
+  lineHeightH6: "15px",
+  // Elevation
+  zIndexToast: 101,
+  zIndexNavBar: 100,
+  zIndexModal: 99,
+  zIndexModalCloseIndex: 100
+};
+
+const createTheme = theme => {
+  return { ...baseTheme,
+    ...theme
+  };
+};
+
+export { Button, Carousel, FAIcon, Loader, Map$1 as Map, Modal, Trail, TrailChart, createTheme, useModal };
 //# sourceMappingURL=index.js.map
