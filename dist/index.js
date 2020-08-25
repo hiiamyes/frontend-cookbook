ed476954016cf404ba25c57f693a9f3955b070da
yes <yes@emq.com>
2020-08-25 17:03:52 +0800

    v1.0.23

--- dist/index.js
+++ dist/index.js
@@ -2652,6 +2652,11 @@ var faMinus = {
   iconName: 'minus',
   icon: [448, 512, [], "f068", "M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"]
 };
+var faMountain = {
+  prefix: 'fas',
+  iconName: 'mountain',
+  icon: [640, 512, [], "f6fc", "M634.92 462.7l-288-448C341.03 5.54 330.89 0 320 0s-21.03 5.54-26.92 14.7l-288 448a32.001 32.001 0 0 0-1.17 32.64A32.004 32.004 0 0 0 32 512h576c11.71 0 22.48-6.39 28.09-16.67a31.983 31.983 0 0 0-1.17-32.63zM320 91.18L405.39 224H320l-64 64-38.06-38.06L320 91.18z"]
+};
 var faMugHot = {
   prefix: 'fas',
   iconName: 'mug-hot',
@@ -2724,6 +2729,7 @@ library.add(faLongArrowAltDown);
 library.add(faMugHot);
 library.add(faAngleDoubleRight);
 library.add(faClock);
+library.add(faMountain);
 
 const FAIcon = props => {
   const {
@@ -3222,8 +3228,11 @@ var Style$5 = styled.div`
 `;
 
 const Loader = props => {
+  const {
+    icon = "spinner"
+  } = props;
   return /*#__PURE__*/React.createElement(Style$5, null, /*#__PURE__*/React.createElement(FAIcon, {
-    icon: "spinner"
+    icon: icon
   }));
 };
 
