c26546efb69564e8b8e7a7a8d3c91cc827da7910
Yes <joshuayes@gmail.com>
2020-09-21 22:46:37 +0800

    v1.0.36

--- dist/index.js
+++ dist/index.js
@@ -8360,10 +8360,14 @@ const drawAxis = ({
 }) => {
   const xAxisTickValueInterval = 1000;
   const xAxisTickValues = [...[...new Array(Math.floor(xMax / xAxisTickValueInterval))].map((_, i) => i * xAxisTickValueInterval), xMax];
-  svg.append("g").attr("class", "x axis").attr("transform", "translate(0," + height + ")").call(axisBottom(xScale).tickValues(xAxisTickValues).tickSize(-height).tickFormat(d => `${(d / 1000).toFixed(1)}k`)).selectAll("text").attr("y", "10px");
+  svg.append("g").attr("class", "x axis").attr("transform", "translate(0," + height + ")").call(axisBottom(xScale).ticks(5) // .tickValues(xAxisTickValues)
+  .tickSize(-height).tickFormat(d => `${(d / 1000).toFixed(1)}k`)).selectAll("text").attr("y", "10px");
+  svg.append("g").attr("class", "x axis").attr("transform", "translate(0," + height + ")").call(axisBottom(xScale).tickValues([xMax]).tickSize(-height).tickFormat(d => `${(d / 1000).toFixed(1)}k`)).selectAll("text").attr("y", "10px");
   const yAxisTickValueInterval = 500;
   const yAxisTickValues = yMax - yMin > yAxisTickValueInterval ? [yMin, ...[...new Array(Math.ceil(yMax / yAxisTickValueInterval) - Math.ceil(yMin / yAxisTickValueInterval))].map((_, i) => (Math.ceil(yMin / yAxisTickValueInterval) + i) * yAxisTickValueInterval), yMax] : [yMin, yMax];
-  svg.append("g").attr("class", "y axis").call(axisLeft(yScale).tickValues(yAxisTickValues).tickSize(-width).tickFormat(d => `${d.toFixed(0)}m`)).selectAll("text").attr("x", "-10px");
+  svg.append("g").attr("class", "y axis").call(axisLeft(yScale).ticks(4) // .tickValues(yAxisTickValues)
+  .tickSize(-width).tickFormat(d => `${d.toFixed(0)}m`)).selectAll("text").attr("x", "-10px");
+  svg.append("g").attr("class", "y axis").call(axisLeft(yScale).tickValues([yMin, yMax]).tickSize(-width).tickFormat(d => `${d.toFixed(0)}m`)).selectAll("text").attr("x", "-10px");
 };
 
 const drawLine = ({
