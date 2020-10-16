7ed2ec9a8903c1df235536b078ed6052e0525f82
Yes <joshuayes@gmail.com>
2020-10-16 23:47:09 +0800

    Add terser

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import babel from "@rollup/plugin-babel";
 import alias from "@rollup/plugin-alias";
+import { terser } from "rollup-plugin-terser";
 import pkg from "./package.json";
 
 export default {
@@ -12,9 +13,19 @@ export default {
       file: pkg.main,
       format: "es",
       sourcemap: true,
+      compact: true,
     },
   ],
-  external: ["react", "react-com", "styled-components", "leaflet", "lodash"],
+  external: [
+    "react",
+    "react-com",
+    "styled-components",
+    "leaflet",
+    "lodash",
+    "d3",
+    "react-router-dom",
+    "classnames",
+  ],
   plugins: [
     resolve(),
     commonjs({
@@ -28,5 +39,6 @@ export default {
         { find: "src", replacement: path.resolve(__dirname, "./src/") },
       ],
     }),
+    terser(),
   ],
 };
